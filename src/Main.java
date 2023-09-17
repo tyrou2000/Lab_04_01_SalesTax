d5468af410f76344f1b794aa08f49e57ca1f44ed
Ty <tyrou2000@gmail.com>
2023-09-17 12:24:44 -0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        double purchasePrice = 6.00;
+        double priceWTax = 0;
+
+        priceWTax = purchasePrice * .05;
+
+        System.out.println("Your sales tax is: " + priceWTax);
+
+
+    }
+}
